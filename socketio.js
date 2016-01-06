ab51568ac1eb8816cc70353c42f8697cf08a9c0f
stardust <610971194@qq.com>
2016-01-06 23:52:30 +0800

    完成单聊邀请的发起

--- socketio.js
+++ socketio.js
@@ -1,58 +1,101 @@
 var fs = require('fs');
 var sessionMiddleware = require('./app').sessionMiddleware;
 
-function on_connect(io) {
-  return function(socket) {
-    var username = socket.request.session.username;
+var online_users = [],
+    available_users = [],
+    io;
 
-    if (!username) return socket.disconnect();
+function on_connect(socket) {
+  var username = socket.request.session.username;
 
-    handle_online_users(username, io);
+  if (!username) return socket.disconnect();
 
-    console.log(username + ' connect successfully');
-
-    socket.on('message', function(msg){
-      io.emit('message', username + ': ' + msg);
-    });
-
-    socket.on('force_disconnect', socket.disconnect);
-
-    socket.on('disconnect', disconnect_socket(username, io));
-  }
-}
-
-function handle_online_users(username, io) {
-  var online_users = JSON.parse(fs.readFileSync('./data/online_users.json'));
+  socket.username = username;
 
   online_users.push(username);
+  console.log('connect ' + JSON.stringify(online_users));
   io.emit('online_users_change', online_users);
-  fs.writeFileSync('./data/online_users.json', JSON.stringify(online_users));
+
+  available_users.push(username);
+  io.emit('available_users_change', available_users);
+
+  console.log(username + ' connect successfully');
+
+  socket.on('message', function(msg){
+    io.emit('message', username + ': ' + msg);
+  });
+
+  socket.on('force_disconnect', socket.disconnect);
+
+  socket.on('disconnect', disconnect_socket(username));
+
+  socket.on('invitation', invite_user(socket));
 }
 
-function disconnect_socket(username, io) {
+function disconnect_socket(username) {
   return function(socket) {
-    var online_users = JSON.parse(fs.readFileSync('./data/online_users.json'));
-
     for (var i = 0; i < online_users.length; i++) {
       if (online_users[i] === username) {
         online_users.splice(i, 1);
-        fs.writeFileSync('./data/online_users.json', JSON.stringify(online_users));
+
+        console.log('disconnect ' + JSON.stringify(online_users));
         io.emit('online_users_change', online_users);
         console.log(username + ' disconnect successfully');
+        break;
+      }
+    }
+
+    for (var i = 0; i < available_users.length; i++) {
+      if (available_users[i] === username) {
+        available_users.splice(i, 1);
+        io.emit('available_users_change', available_users);
+        break;
+      }
+    }
+
+  }
+}
+
+function invite_user(inviter_socket) {
+  return function(invitee) {
+    var sockets = io.sockets.sockets,
+        invitee_id;
+
+    console.log('invite: ' + invitee);
+
+    if (inviter_socket.invitee) return;
+
+    for (var i = 0; i < sockets.length; i++) {
+      if (sockets[i].username === invitee) {
+        invitee_id = sockets[i].id;
+        io.to(invitee_id).emit('invitation', inviter_socket.username);
+
         return;
       }
     }
+
+    io.to(inviter_socket.id).emit('invitation_error');
+  }
+}
+
+function cancel_invitation(socket) {
+  return function() {
+
+    if (!socket.invitee) return;
+
+    console.log('cancel_invitation: ' + socket.username);
+
+    io.to(socket.invitee).emit('cancel_invitation', socket.username);
+    delete socket.invitee;
   }
 }
 
 module.exports = function(server) {
-  var io = require('socket.io')(server);
-
-  fs.writeFileSync('./data/online_users.json', '[]');
+  io = require('socket.io')(server);
 
   io.use(function(socket, next) {
       sessionMiddleware(socket.request, socket.request.res, next);
   });
 
-  io.on('connection', on_connect(io));
+  io.on('connection', on_connect);
 }
--- views/room/room.js
+++ views/room/room.js
@@ -5,12 +5,23 @@ $(function() {
   socket = io(window.location.href);
 
   $('.logout').click(logout);
-
   $('.input-area form').submit(send_message);
 
   socket.on('message', append_message);
+  socket.on('online_users_change',
+    change_user_list('.online-user-list', '.online-user-item'));
 
-  socket.on('online_users_change', change_user_list);
+  $(document).on('click', '.available-user-item', select_available_user);
+  $(document).click(cancel_select);
+  $('.invite-btn').click(invite_user);
+  $('.accept-btn').click(accept_invitation);
+  $('.refuse-btn').click(refuse_invitation);
+
+
+  socket.on('available_users_change',
+    change_user_list('.available-user-list', '.available-user-item'));
+  socket.on('invitation', get_invitation());
+
 });
 
 function logout() {
@@ -41,16 +52,69 @@ function append_message(msg) {
   $(".message-area").scrollTop($(".message-area")[0].scrollHeight);
 }
 
-function change_user_list(users) {
-  var user_list = $('.user-list'),
-      user_item_template = $('.user-item.template').clone().removeClass('template'),
-      user_item;
 
-  user_list.empty();
+function select_available_user() {
+  $('.available-user-item').removeClass('selected');
+  $(this).addClass('selected');
 
-  for (var i = 0 ; i < users.length; i++) {
-    user_item = user_item_template.clone().text(users[i]);
-    user_list.append(user_item);
+  return false;
+}
+
+
+function change_user_list(user_list_str, user_item_str) {
+  return function change_available_user_list(users) {
+    var user_list = $(user_list_str),
+        user_item_template = $(user_item_str + '.template').clone().removeClass('template'),
+        username = $('.username').text(),
+        user_item;
+
+    $(user_item_str).not('.template').remove();
+
+    for (var i = 0 ; i < users.length; i++) {
+      if (users[i] === username) continue;
+
+      user_item = user_item_template.clone().text(users[i]);
+      user_list.append(user_item);
+    }
   }
+}
 
+function cancel_select() {
+  $('.selected').removeClass('selected');
+}
+
+function invite_user() {
+  var invite_btn = $(this),
+      selected_user;
+
+  if ($('.available-user-item.selected').length === 0) return false;
+
+  selected_user = $('.available-user-item.selected').text();
+  socket.emit('invitation', selected_user);
+
+  return false;
+}
+
+function accept_invitation() {
+  return false;
+}
+
+function refuse_invitation() {
+  return false;
+}
+
+function get_invitation() {
+  var invitations = [];
+
+  return function(inviter) {
+    console.log('inviter: ' + inviter);
+
+    for (var i = 0; i < invitations.length; i++) {
+      if (invitations[i] === inviter) return;
+    }
+
+    invitations.push(inviter);
+
+    change_user_list('.inviting-user-list', '.inviting-user-item')(invitations);
+  }
 }
